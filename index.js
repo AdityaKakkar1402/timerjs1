9c3397d8b3ecc46953ae0b15368b4e455a87c869
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-01-26 17:41:49 +0530

    input date

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-const birthDay="14 Feb 2022";
+// const birthDay="14 Feb 2022";
 
  daysid=document.getElementById('days');
  hoursid=document.getElementById('hours');
  minid=document.getElementById('min');
  secid=document.getElementById('sec');
+ givenDate=document.getElementById("dateInput");
+ buttonDate=document.getElementById("buttonInput");
 
 
 
 function countDown(){
-    const birth= new Date(birthDay);
+    var dateVal=new Date(givenDate.value);
+    dateVal.setHours(0,0,0,0);
+    
+
+    
+    // const birth= new Date(birthDay);
     const curDate= new Date();
-    const sec=(birth-curDate)/1000;
+    const sec=(dateVal-curDate)/1000;
     const days=Math.floor(sec/(3600*24));
     const hours=Math.floor(sec/3600)%24;
     const mins=Math.floor(sec/60)%60;
     const secs=Math.floor(sec)%60
+    
     
     daysid.innerHTML=days;
     hoursid.innerHTML=hours;
     minid.innerHTML=mins;
     secid.innerHTML=secs;
 
+    console.log(dateVal);
+    console.log(curDate);
+    // console.log(birth);
 
-    console.log(days,hours,mins,secs);
+
+   
 }
 
-setInterval(countDown, 1000)
+buttonDate.addEventListener("click",function(){
+    setInterval(countDown,1000);
+});
+
